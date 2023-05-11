7ef39db0977b63f60611310b325b1453b4e7aca5
rolandexplore93 <roland2rule@gmail.com>
2023-05-11 18:15:35 +0000

    Deployment issues: added swagger.js

---
+++ swagger.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const adminRoutesDocs = require('./routes/admin.swaggerdocs');
+const router = express.Router();
+
+// const app = express();
+
+// const options = {
+//     definition: {
+//         openapi: '3.0.0',
+//         info: {
+//             title: 'Wealth Affairs Inc API',
+//             version: '1.0.0',
+//             description: 'Wealth Affairs API documentation'
+//         },
+//         servers: [
+//             {
+//                 url: 'http://localhost:3000',
+//             },
+//         ],
+//     },
+//     apis: ['./index.js', './routes/*.js']
+// };
+
+// const swaggerSpecs = swaggerJsdoc(options);
+// module.exports = { swaggerSpecs, swaggerUi };
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Wealth Affairs Inc API',
+            version: '1.0.0',
+            description: 'Wealth Affairs API documentation',
+            contact: {
+                email: 'roland2rule@gmail.com'
+            },
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+        tags: [
+            {
+                name: 'Admin',
+                description: 'Admin routes'
+            }
+        ],
+        paths: {...adminRoutesDocs}
+    },
+    apis: ['./index.js', './routes/*.js']
+};
+
+const swaggerSpecs = swaggerJsdoc(options); // Initialising swagger specs
+router.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+module.exports = router;
